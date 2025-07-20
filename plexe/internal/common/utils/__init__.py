3a95cd7354807d7208ee97be27041a601829f30d
Polymath: Shayne-Edward: Sovereign Monarch: of the Family Chaney | ❂ ✹ <91806248+Gillithe@users.noreply.github.com>
2025-07-20 14:07:41 -0400

    Add asynchronous agent manager utility

--- plexe/internal/common/utils/__init__.py
+++ plexe/internal/common/utils/__init__.py
@@ -1,3 +1,5 @@
-"""
-Common utility functions for the plexe library.
-"""
+"""Common utility functions for the plexe library."""
+
+from .async_agents import run_agents_concurrently
+
+__all__ = ["run_agents_concurrently"]
